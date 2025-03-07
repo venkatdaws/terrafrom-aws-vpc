e0975773317753269d54dfd5c2411c26ba277edf
Venkat <reddy738@gmail.com>
2025-03-07 14:06:37 +0530

    vpc_igw_id

--- output.tf
+++ output.tf
@@ -19,5 +19,5 @@ output "database_subnet_group_id"{
     value = aws_db_subnet_group.default.id
 }
 output "igw_id"{
-    value = aws_internet_gateway.gw.id
+    value = aws_internet_gateway.igw_id
 }
