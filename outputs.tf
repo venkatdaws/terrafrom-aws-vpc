d018ef995278f24fb0e3b3e98d72b2d43d48f11e
Venkat <reddy738@gmail.com>
2025-03-08 20:36:32 +0530

    sg

--- outputs.tf
+++ outputs.tf
@@ -18,6 +18,9 @@ output "database_subnet_ids"{
 output "database_subnet_group_id"{
     value = aws_db_subnet_group.default.id
 }
+output "database_subnet_group_name"{
+    value = aws_db_subnet_group.default.name
+}
 output "igw_id" {
   value = aws_internet_gateway.gw.id
 }
